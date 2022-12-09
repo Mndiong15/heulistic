19f45d413aa73597200a240fec92173b550cc1d7
mndiong <51433178+Mndiong15@users.noreply.github.com>
2022-12-09 12:22:30 -0500

    Gunicorn connectiity
    Connecting React with Django

---
+++ Procfile
@@ -0,0 +1,2 @@
+
+web: gunicorn heulisticai.wsgi
--- app/urls.py
+++ app/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from django.views.generic import TemplateView
 from . import views
 
 # URL Configuration
 urlpatterns = [
-    path('hello/', views.say_hello)
+    path('hello/', views.say_hello),
+    path('', TemplateView.as_view(template_name="index.html")),
 ]
